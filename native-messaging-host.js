a4dff6c8d497f730dfe8ed39bc7ad8b5f2d1068e
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-04 10:09:31 +0200

    Create native-messaging-host.js

---
+++ native-messaging-host.js
@@ -0,0 +1,6 @@
+// Simple implementation
+// on invocation returns channel sdp 
+// designed to start indipendent instances
+
+// Advanced Implementation
+// Single running instance that keeps track of running jobs
